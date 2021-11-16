863b0ae5e0ccc681b703b61b3fddf3f3d3614b1e
Rhys Smith <rhys.smith@audacia.co.uk>
2021-11-16 11:52:54 +0000

    Initial complier commit

---
+++ Audacia.Quest/Audacia.Quest.Compiler/Complier.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Audacia.Quest.Compiler;
+using Microsoft.CSharp;
+
+namespace Audacia.Quest.Compiler;
+
+public class Compiler : ICompiler
+{
+    private readonly CSharpCodeProvider _codeProvider = new CSharpCodeProvider();
+
+    private readonly ICodeCompiler _codeCompiler;
+    
+    /// <summary>
+    /// I think these should come from options -> as we could be about using different types of compilers (js, ts)
+    /// </summary>
+    private readonly CompilerParameters _compilerParameters;
+
+    public string Output { get; set; }
+    
+    public Compiler()
+    {
+        _codeCompiler = _codeProvider.CreateCompiler();
+
+        _compilerParameters = new CompilerParameters();
+        _compilerParameters.GenerateExecutable = true;
+        _compilerParameters.OutputAssembly = Output;
+    }
+
+    public string ConvertToSource(string code)
+    {
+        return code;
+    }
+
+    public string Compile(string source)
+    {
+        var compilerResult = _codeCompiler.CompileAssemblyFromSource(_compilerParameters, source);
+
+        if (compilerResult.Errors.HasErrors)
+        {
+            LogErrors(compilerResult.Errors);
+        }
+        else
+        {
+            Console.WriteLine(compilerResult.Output);
+
+            ExecuteCompiledCode();
+        }
+
+        return string.Empty;
+    }
+
+    private void ExecuteCompiledCode()
+    {
+        Output = Guid.NewGuid().ToString() + ".exe";
+        var processStartInfo = new ProcessStartInfo()
+        {
+            FileName = Output,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
+
+        var process = new Process()
+        {
+            StartInfo = processStartInfo
+        };
+
+        process.Start();
+        while (!process.StandardOutput.EndOfStream)
+        {
+            var executedResult = process.StandardOutput.ReadLine();
+            
+            // this should be equal to 'Hello World!' using the MainExample
+            Console.WriteLine($"Compile successful: {executedResult == "Hello World!"}");
+        }
+    }
+
+    private void LogErrors(CompilerErrorCollection compilerErrorCollection)
+    {
+        foreach (CompilerError compilerError in compilerErrorCollection)
+        {
+            Console.WriteLine("Line number " + compilerError.Line +
+                              ", Error Number: " + compilerError.ErrorNumber +
+                              ", '" + compilerError.ErrorText + ";");
+        }
+    }
+}
---
+++ Audacia.Quest/Audacia.Quest.Compiler/ICompiler.cs
@@ -0,0 +1,19 @@
+﻿namespace Audacia.Quest.Compiler
+{
+    public interface ICompiler
+    {
+        /// <summary>
+        /// This is the code sent from the client.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        string ConvertToSource(string code);
+        
+        /// <summary>
+        /// Compiles for the code with the string and returns a result.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        string Compile(string source);
+    }
+}
